ca04b596e81997244f7a4ade504f419afb48b89b
Adir <adiram33@gmail.com>
2024-08-26 01:59:35 +0300

    Fixed CSS issue and resolved merge conflicts

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export class User{
      name : string;
      email : string;
@@ -61,70 +60,4 @@ export class User{
                ", gender: " + this.gender +
                ", dateOfBirth: " + this.dateOfBirth
     }
-
-=======
-export class User{
-     name : string;
-     email : string;
-     password : string;
-     img !: string;
-     gender : string;
-     dateOfBirth : string;
-    
-    constructor(name : string, email : string, password : string, gender : string, dateOfBirth : string){
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.gender = gender;
-        this.dateOfBirth = dateOfBirth;
-        this.setImg();
-    }
-    getName(): string {
-        return this.name;
-    }
-    setName(name: string) {
-        this.name = name;
-    }
-    getEmail(): string {
-        return this.email;
-    }
-    setEmail(email: string) {
-        this.email = email;
-    }
-    getPassword(): string {
-        return this.password;
-    }
-    setPassword(password: string) {
-        this.password = password;
-    }
-    getImg() :string {
-        return this.img;
-    }
-    setImg() {
-        if(this.gender=="Male"||this.gender=="male") this.img = "/assets/img/ManAvatar.png";
-        else this.img = "/assets/img/WomanAvatar.png";
-    }
-    getGender(): string {
-        return this.gender;
-    }
-    setGender(gender: string) {
-        this.gender = gender;
-        this.setImg();
-    }
-    getDateOfBirth(): string {
-        return this.dateOfBirth;
-    }
-    setDateOfBirth(dateOfBirth : string) {
-        this.dateOfBirth = dateOfBirth;
-    }
-    toString(): string {
-        return ", name: " + this.name + 
-               ", email: " + this.email + 
-               ", password: " + this.password + 
-               ", img: " + this.img + 
-               ", gender: " + this.gender +
-               ", dateOfBirth: " + this.dateOfBirth
-    }
-
->>>>>>> be96b3614cab7aadb259689a616faf125233c34a
 }
